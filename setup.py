e1cb132d7bef2d72f86ada700285a2c52a129bb0
piglet94 <715654107@qq.com>
2021-11-22 12:30:31 +0800

    6-2

--- setup.py
+++ setup.py
@@ -24,12 +24,6 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=[
-        'numpy','argparse','matplotlib','scipy','os'
-        ],
-    entry_points={
-        'console_scripts': [
-            'simplelayout=simplelayout:main',
-        ],
-    },
+    install_requires=['numpy','argparse','matplotlib','scipy','os'],
+    entry_points={'console_scripts': ['simplelayout=simplelayout:main',],},
 )
